d1dd61a197efe122f51bc831e3d469519dce50f1
dinhduc24k322 <dinhducphan02@gmail.com>
2024-03-19 16:41:45 +0700

    Thêm Command và Responsitory

---
+++ Netflix2/Controllers/Command/IUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using Netflix2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix2.Controllers.Command
+{
+    public interface IUserCommand
+    {
+        void Execute(KhachHang user);
+    }
+}
---
+++ Netflix2/Controllers/Responsitory/KhachHangRepository.cs
@@ -0,0 +1,34 @@
+﻿using Netflix2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Netflix2.Controllers.Responsitory
+{
+    public class KhachHangRepository
+    {
+        private XemPhimEntities database;
+
+        public KhachHangRepository()
+        {
+            database = new XemPhimEntities();
+        }
+
+        public KhachHang GetById(int id)
+        {
+            return database.KhachHang.FirstOrDefault(i => i.MaKH == id);
+        }
+
+        public void Delete(KhachHang entity)
+        {
+            database.KhachHang.Remove(entity);
+            database.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            database.Dispose();
+        }
+    }
+}
