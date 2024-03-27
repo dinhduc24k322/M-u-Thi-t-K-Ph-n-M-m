64fb2052779370cdde268ff1e4e0cd17072ef05d
dinhduc24k322 <dinhducphan02@gmail.com>
2024-03-27 17:50:47 +0700

    Thêm chức năng mới gắn với mẫu thiết kế Proxy

---
+++ Netflix2/Controllers/Proxy/IMoviePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Trình_chiếu_xem_phim
+{
+     interface IMoviePlayer
+    {
+        void PlayMovie(string movieName);
+    }
+}
---
+++ Netflix2/Controllers/Proxy/RealMoviePlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Trình_chiếu_xem_phim
+{
+    class RealMoviePlayer : IMoviePlayer
+    {
+        public void PlayMovie(string movieName)
+        {
+            Console.WriteLine($"Playing movie: {movieName}");
+        }
+    }
+}
