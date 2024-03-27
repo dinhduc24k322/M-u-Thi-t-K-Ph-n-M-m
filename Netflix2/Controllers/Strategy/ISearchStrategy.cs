21d6f96e51a84376461b76d47029a3b2f2a2160d
dinhduc24k322 <dinhducphan02@gmail.com>
2024-03-27 15:58:31 +0700

    Thêm mẫu Strategy

---
+++ Netflix2/Controllers/Strategy/ISearchStrategy.cs
@@ -0,0 +1,21 @@
+﻿using Netflix2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix2.Controllers.Strategy
+{
+    public interface ISearchStrategy
+    {
+        List<Phim> Search(string searchString, XemPhimEntities database);
+    }
+    public class TenPhimSearchStrategy : ISearchStrategy
+    {
+        public List<Phim> Search(string searchString, XemPhimEntities database)
+        {
+            return database.Phim.Where(p => p.TenPhim.Contains(searchString)).ToList();
+        }
+    }
+}
---
+++ Netflix2/Controllers/Strategy/TheLoaiSearchStrategy.cs
@@ -0,0 +1,16 @@
+﻿using Netflix2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Netflix2.Controllers.Strategy
+{
+    public class TheLoaiSearchStrategy : ISearchStrategy
+    {
+        public List<Phim> Search(string searchString, XemPhimEntities database)
+        {
+            return database.Phim.Where(p => p.TheLoai.Contains(searchString)).ToList();
+        }
+    }
+}
